e5dc27df8a85ad5a61f5d71173921514924cf4ac
MvAlen0304 <120381989+MvAlen0304@users.noreply.github.com>
2024-03-19 12:48:05 +0100

    versie 2

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
 console.log("Het is gelukt!")
+
+const faceBtn = document.getElementById(faceBtn)
+
+function skinChange() {
+    document.getElementById('face').src='./img/svg/faceDark.svg'
+}
+
+faceBtn.addEventListener("click", skinChange)
