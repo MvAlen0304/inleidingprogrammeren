faa6dd20f25f7ee1e7453de358dfbdbc2e5c327c
MvAlen0304 <120381989+MvAlen0304@users.noreply.github.com>
2024-03-21 15:47:18 +0100

    versie 4

--- scripts.js
+++ scripts.js
@@ -1,27 +1,54 @@
 console.log("Het is gelukt!")
 
-const lightBtn = document.getElementById("lightBtn")
-const mediumLBtn = document.getElementById("mediumLBtn")
-const mediumDBtn = document.getElementById("mediumDBtn")
-const darkBtn = document.getElementById("darkBtn")
+// const lightBtn = document.getElementById("lightBtn")
+// const mediumLBtn = document.getElementById("mediumLBtn")
+// const mediumDBtn = document.getElementById("mediumDBtn")
+// const darkBtn = document.getElementById("darkBtn")
 
-function skinChangeLight() {
-    document.getElementById('face').src='./img/svg/faceLight.svg'
+// function skinChangeLight() {
+//     document.getElementById('face').src='./img/svg/faceLight.svg'
+// }
+
+// function skinChangeMediumL() {
+//     document.getElementById('face').src='./img/svg/faceLightM.svg'
+// }
+
+// function skinChangeMediumD() {
+//     document.getElementById('face').src='./img/svg/faceDarkM.svg'
+// }
+
+// function skinChangeDark() {
+//     document.getElementById('face').src='./img/svg/faceDark.svg'
+// }
+
+function skinChange(event) {
+    console.log(event)
+    const buttonId = event.target.id;
+    
+    if (buttonId === 'faceLight') {
+        document.getElementById('face').src = './img/svg/' + buttonId + '.svg';
+    } else if (buttonId === 'faceLightM') {
+        document.getElementById('face').src = './img/svg/faceLightM.svg';
+    } else if (buttonId === 'faceDarkM') {
+        document.getElementById('face').src = './img/svg/faceDarkM.svg';
+    } else if (buttonId === 'faceDark') {
+        document.getElementById('face').src = './img/svg/faceDark.svg';
+    }
 }
 
-function skinChangeMediumL() {
-    document.getElementById('face').src='./img/svg/faceLightM.svg'
+function eyeChange(event) {
+    const buttonId = event.target.id;
+
+    if (buttonId === 'eyesBlue') {
+        document.getElementById('eyes').src = './img/svg/eyesBlue.svg';
+    } else if (buttonId === 'eyesGreen') {
+        document.getElementById('eyes').src = './img/svg/eyesGreen.svg';
+    } else if (buttonId === 'eyesBrown') {
+        document.getElementById('eyes').src = './img/svg/eyesBrown.svg';
+    }
 }
 
-function skinChangeMediumD() {
-    document.getElementById('face').src='./img/svg/faceDarkM.svg'
-}
+document.querySelector('.faceBtns').addEventListener("click", skinChange);
 
-function skinChangeDark() {
-    document.getElementById('face').src='./img/svg/faceDark.svg'
-}
+document.querySelector('.eyeBtns').addEventListener("click", eyeChange)
 
-lightBtn.addEventListener("click", skinChangeLight)
-mediumLBtn.addEventListener("click", skinChangeMediumL)
-mediumDBtn.addEventListener("click", skinChangeMediumD)
-darkBtn.addEventListener("click", skinChangeDark)
