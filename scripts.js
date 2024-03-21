a55c3852fbac3ffb722c1842e37f3221a917a20f
MvAlen0304 <120381989+MvAlen0304@users.noreply.github.com>
2024-03-21 11:03:15 +0100

    versie 3

--- scripts.js
+++ scripts.js
@@ -1,9 +1,27 @@
 console.log("Het is gelukt!")
 
-const faceBtn = document.getElementById(faceBtn)
+const lightBtn = document.getElementById("lightBtn")
+const mediumLBtn = document.getElementById("mediumLBtn")
+const mediumDBtn = document.getElementById("mediumDBtn")
+const darkBtn = document.getElementById("darkBtn")
 
-function skinChange() {
+function skinChangeLight() {
+    document.getElementById('face').src='./img/svg/faceLight.svg'
+}
+
+function skinChangeMediumL() {
+    document.getElementById('face').src='./img/svg/faceLightM.svg'
+}
+
+function skinChangeMediumD() {
+    document.getElementById('face').src='./img/svg/faceDarkM.svg'
+}
+
+function skinChangeDark() {
     document.getElementById('face').src='./img/svg/faceDark.svg'
 }
 
-faceBtn.addEventListener("click", skinChange)
+lightBtn.addEventListener("click", skinChangeLight)
+mediumLBtn.addEventListener("click", skinChangeMediumL)
+mediumDBtn.addEventListener("click", skinChangeMediumD)
+darkBtn.addEventListener("click", skinChangeDark)
